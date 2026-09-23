3b25cb7d18ff2ac8e7aa3108ca247e3d05c10498
Andy Doan <andy@foundries.io>
2026-09-23 14:24:55 -0500

    initial commit
    Signed-off-by: Andy Doan <andy@foundries.io>

---
+++ build-intel.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd $(dirname $(readlink -f $0))
+
+#builddir=`pwd`/.build
+builddir=/home/fio/build-tmp-delete
+export KAS_WORK_DIR="${builddir}/work"
+export DL_DIR="${builddir}/downloads"
+export SSTATE_DIR="${builddir}/sstate-cache"
+mkdir -p "${DL_DIR}" "${SSTATE_DIR}" "${KAS_WORK_DIR}"
+
+action="${1:-build}"
+if (( $# > 0 )); then
+    shift
+fi
+
+exec ./kas-container "${action}" intel.yml "$@"
